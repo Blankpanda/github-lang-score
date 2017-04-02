83a636b28609a05f4a1120c385ed9933e2b6fbc2
blankpanda <blankpanda8@gmail.com>
2017-04-02 16:33:07 -0400

    update. api get broken

--- scoringGet/score.js
+++ scoringGet/score.js
@@ -3,7 +3,7 @@ var request = require("request");
 var GitHubApi = require("github");
 var auth = require("./creds.json"); 
 
-var languages = [];
+var languages = {};
 
 var github = new GitHubApi({
     headers: {
@@ -20,19 +20,51 @@ github.authenticate({
 
 github.repos.getAll({
     username: "blankpanda"
-}, function(err, res) {
+}, function(err, res) {    
     var repos = res.data;
     for(var i = 0; i < repos.length;i++) {
+        
 	github.repos.getLanguages({
 	    owner: "blankpanda",
 	    repo: repos[i].name,
 	}, function (err, res) {
+            
 	    if(res != undefined) {
-		console.log(res.data);
+                aggregateLang(res.data); // TODO: Prune repeats
 	    }
-
 	});
     }
-
  });
 
+function aggregateLang(json) {
+    //    json = pruneRepeats(json);
+    var cc = 0;
+    for(attr in json) {
+//         if(!(inLanguages(attr))) {
+//             console.log(attr);
+// //            languages[attr] += json[attr];
+        //         }
+        console.log(attr + ": " + cc);
+        cc++
+    }
+    console.log("\n");
+}
+
+function pruneRepeats(json) {
+
+}
+function inLanguages(attribute) {
+    for(attr in languages) {
+        if(attr == attribute) {
+            return true;        
+        }
+    }
+    return false;
+}
+
+
+function printLanguages() {
+    for(attr in languages) {
+        console.log(attr + ": " + languages[attr]);
+    }
+}
